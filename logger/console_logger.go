5af8d4da0a09fb3f6cfcbc99871d9a2d2fafa1e7
khivuksergey <khivuksergey@gmail.com>
2024-04-24 23:59:22 +0500

    fix: Logger methods target is pointer

--- logger/console_logger.go
+++ logger/console_logger.go
@@ -12,38 +12,38 @@ func NewConsoleLogger() Logger {
 	}
 }
 
-func (l logger) SetLevel(level LogLevel) {
+func (l *logger) SetLevel(level LogLevel) {
 	l.level = level
 }
 
-func (l logger) Debug(message LogMessage) {
+func (l *logger) Debug(message LogMessage) {
 	if l.level > Debug {
 		return
 	}
 	fmt.Printf(message.String(), "DEBUG")
 }
 
-func (l logger) Info(message LogMessage) {
+func (l *logger) Info(message LogMessage) {
 	if l.level > Info {
 		return
 	}
 	fmt.Printf(message.String(), "INFO")
 }
 
-func (l logger) Warn(message LogMessage) {
+func (l *logger) Warn(message LogMessage) {
 	if l.level > Warn {
 		return
 	}
 	fmt.Printf(message.String(), "WARN")
 }
 
-func (l logger) Error(message LogMessage) {
+func (l *logger) Error(message LogMessage) {
 	if l.level > Error {
 		return
 	}
 	fmt.Printf(message.String(), "ERROR")
 }
 
-func (l logger) Fatal(message LogMessage) {
+func (l *logger) Fatal(message LogMessage) {
 	fmt.Printf(message.String(), "FATAL")
 }
